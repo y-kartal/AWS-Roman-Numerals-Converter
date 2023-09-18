67d94cb236a522597bedec5503c49b07e21ea9fb
ykartal2 <yasinkartal2009@gmail.com>
2023-09-18 21:59:02 +0300

    third

--- roman-numerals-converter-app.py
+++ roman-numerals-converter-app.py
@@ -12,11 +12,19 @@ def convert(decimal_num):
     return num_to_roman
 
 
-
-
-
-
-
+@app.route('/', methods=['POST', 'GET'])
+def main_post():
+    if request.method == 'POST':
+        alpha = request.form['number']
+        if not alpha.isdecimal():
+            return render_template('index.html', developer_name='Yasin KARTAL', not_valid=True)
+        number = int(alpha)
+        if not 0 < number < 4000:
+            return render_template('index.html', developer_name='Yasin KARTAL', not_valid=True)
+        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Yasin KARTAL')
+    else:
+        return render_template('index.html', developer_name='Yasin KARTAL', not_valid=False)
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
