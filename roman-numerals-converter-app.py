0d3d8cfa8d00750e9dee39db16e584f7e55df57a
ykartal2 <yasinkartal2009@gmail.com>
2023-09-18 20:53:00 +0300

    second

--- roman-numerals-converter-app.py
+++ roman-numerals-converter-app.py
@@ -2,16 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def convert(decimal_num):
+    roman = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
+    num_to_roman = ''
 
-
-
-
-
-
-
-
-
-
+    for i in roman.keys():
+        num_to_roman += roman[i]*(decimal_num//i) 
+        decimal_num %= i
+    return num_to_roman
 
 
 
